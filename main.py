782c48d7f591523ea6f9a6c68eb5de06a7fb8bd1
CherylChao <cherry885930@yahoo.com.tw>
2023-05-22 22:44:15 +0800

    update test_collide_zombie 0522

--- main.py
+++ main.py
@@ -84,6 +84,7 @@ class Game:
         #extra params for testing
         self.hit_test = None
         self.hit_test_bonus = None
+        self.hit_test_zb = None
 
     def load_data(self):
         self.dim_screen.set_alpha(80)
@@ -229,7 +230,7 @@ class Game:
     def _collide_player_with_zombie(self):
         hits = pg.sprite.spritecollide(self.player, self.zombies, False, collide_hit_rect)
         #print(self.zombies)
-        for hit in hits:
+        for hit in self.hit_test_zb:
             self.player.shield -= self.damage
             hit.vel = vector(0, 0)
             if random() < 0.5:
@@ -245,9 +246,9 @@ class Game:
                 else:
                     self.playing = False
                     self.menu.game_over(self.score_list, 'GAME OVER')
-        if hits:
+        if self.hit_test_zb:
             get_hit(self.player)
-            self.player.position += vector(KICKBACK, 0).rotate(-hits[0].rotation)
+            #self.player.position += vector(KICKBACK, 0).rotate(-hits[0].rotation)
 
     def _collide_bullet_with_zombie(self):
         hits = pg.sprite.groupcollide(self.zombies, self.bullets, False, True)
--- test_load.py
+++ test_load.py
@@ -151,24 +151,7 @@ class TestGame(unittest.TestCase):
             else:
                 self.assertEqual(self.game.bullet_images[sets].get_size(),(3,7))
 
-    #Should this be written in another class?
-    # def test_collide_items(self):
-        
-    #     #stub the players' shield
-    #     #self.game._collide_player_with_items()
-
-    #     #self.player.shield = 50
-    #     print('Testing Collision')
-        
-    #     #stubing hits
-    #     hits = self.game.hit_test
-    #     print(hits)
-    #     for hit in hits:
-    #         print(hit.type)
-    #         if hit.type == 'pistol':#
-    #             self.assertIn('pistolll',self.player.all_weapons)
-    #             self.assertEqual(self.player.weapon,'pistol')
-
+    
 
 
 
@@ -185,50 +168,7 @@ class TestGame(unittest.TestCase):
 
 
     
-# class TestMenu()
 
-
-
-
-
-
-# class TestBullet(unittest.TestCase):
-#     def setUp(self):
-#         pg.init()
-#         self.game = pg.sprite.Group()
-#         self.all_sprites = pg.sprite.Group()
-#         self.bullets = pg.sprite.Group()
-#         self.player = pg.sprite.Sprite()
-#         self.player.weapon = "pistol"
-#         self.game.all_sprites = self.all_sprites
-#         self.game.bullets = self.bullets
-#         self.game.player = self.player
-#         self.game.dt = 1 / FPS
-#         self.game.bullet_images = {"large": pg.Surface((10, 10))}
-#         self.bullet = Bullet(self.game, (0, 0), pg.math.Vector2(1, 0))
-
-#     def tearDown(self):
-#         pg.quit()
-
-#     def test_init(self):
-#         self.assertIsInstance(self.bullet, pg.sprite.Sprite)
-#         self.assertIn(self.bullet, self.all_sprites)
-#         self.assertIn(self.bullet, self.bullets)
-#         self.assertEqual(self.bullet.image, self.game.bullet_images[WEAPONS[self.player.weapon]['bullet_size']])
-#         self.assertEqual(self.bullet.position, pg.math.Vector2(0, 0))
-#         self.assertEqual(self.bullet.vel, pg.math.Vector2(WEAPONS[self.player.weapon]['bullet_speed'], 0))
-
-#     def test_update(self):
-#         self.bullet.update()
-#         self.assertEqual(self.bullet.position, pg.math.Vector2(WEAPONS[self.player.weapon]['bullet_speed'], 0))
-
-#     def test_collision_with_wall(self):
-#         wall = pg.sprite.Sprite()
-#         wall.rect = pg.Rect(0, 0, 10, 10)
-#         self.game.walls = pg.sprite.Group(wall)
-#         self.bullet.update()
-#         self.assertNotIn(self.bullet, self.all_sprites)
-#         self.assertNotIn(self.bullet, self.bullets)
 
 if __name__ == '__main__':
     unittest.main()

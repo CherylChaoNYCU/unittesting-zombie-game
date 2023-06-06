f3e03a84d5d16c74adca61f577045f7eb8a924ee
BingJingXie <114390528+BingJingXie@users.noreply.github.com>
2023-06-06 14:28:02 +0800

    Update test_functions.py

--- test_functions.py
+++ test_functions.py
@@ -11,13 +11,12 @@ from zombie_game.item import *
 from zombie_game.menu import *
 from zombie_game.functions import *
 from zombie_game.zombie import *
+from zombie_game.board import *
 from os import path
 import os
 
 
-class Testfunctions(unittest.TestCase):
-    # def setup(self):
-        
+class Testfunctions(unittest.TestCase):        
     def test_collide_hit_rect(self):
         one = Mock()
         two = Mock()
@@ -45,6 +44,20 @@ class Testfunctions(unittest.TestCase):
         
         self.assertEqual(list(character.damage_alpha), expected_alpha)
         self.assertTrue(character.damaged)
+
+    def test_draw_player_health(self):
+        self.game=Game()
+        pg.mixer.pre_init(44100, 16, 1, 2048)
+        pg.init()
+        self.game.board = Board(SCREEN_WIDTH, SCREEN_HEIGHT)
+        print(pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32))
+        
+        draw_player_health(pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32), 20, 10, 0.7)
+        surface = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        
+        self.assertIsNotNone(pg.draw.rect(surface, (0, 255, 0), pg.Rect(20, 10, 70, 20)))
+
+
 
    
 if __name__ == "__main__":
